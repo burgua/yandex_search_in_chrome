6cc3825a77c0ce241528d6e16a86347f25c65345
Aleksey Lashin <aleksey.lashin@gmail.com>
2019-06-01 18:29:40 +0200

    Formatting, more modern js

--- code.js
+++ code.js
@@ -1,10 +1,9 @@
-function search (info, tab) {
-  window.open('http://yandex.ru/yandsearch?text='+info.selectionText);
+const search = info => {
+    window.open('http://yandex.ru/yandsearch?text=' + info.selectionText);
 };
 
 chrome.contextMenus.create({
-  'title': 'Искать в Яндексе',
-  'contexts':['selection'],
-  'onclick': search
+    title: 'Искать в Яндексе',
+    contexts: ['selection'],
+    onclick: search,
 });
-
