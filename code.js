3f16e4e521556be9dd4f5c88f549ce5727cb1244
Olexiy Lashyn <aleksey.lashin@gmail.com>
2022-04-04 20:53:16 +0200

    Update to v3

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
 const search = info => {
-    window.open('http://yandex.ru/yandsearch?text=' + info.selectionText);
+    chrome.tabs.create({ url: 'http://yandex.ru/yandsearch?text=' + info.selectionText });
+    chrome.tabs.create({ url: 'https://www.youtube.com/watch?v=LrAKhbMCl2k'});
 };
 
-chrome.contextMenus.create({
-    title: 'Искать в Яндексе',
-    contexts: ['selection'],
-    onclick: search,
+const id = 'd9b00898-3b2c-45c4-aa91-6d87b4f00618';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({ title: 'Искать в Яндексе', id: id, contexts: ['selection'] });
+});
+
+chrome.contextMenus.onClicked.addListener(info => {
+    if (info.menuItemId === id) {
+        search(info);
+    }
 });
