52be23be0b6ae2aae097348d2f43487b96d5b1bf
SupinePandora43 <supinepandoraggg@gmail.com>
2022-04-21 16:57:53 +0500

    Remove youtube opening

--- code.js
+++ code.js
@@ -1,6 +1,5 @@
 const search = info => {
     chrome.tabs.create({ url: 'http://yandex.ru/yandsearch?text=' + info.selectionText });
-    chrome.tabs.create({ url: 'https://www.youtube.com/watch?v=JNk0ydKNt5w'});
 };
 
 const id = 'd9b00898-3b2c-45c4-aa91-6d87b4f00618';
